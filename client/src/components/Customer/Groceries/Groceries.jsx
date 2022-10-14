2911e44e0878d6b49a27ccb257e7b424fff0aa5b
ThienBk <thiennguyen2000qwer@gmail.com>
2022-10-14 16:22:50 +0700

    14/10/2022

--- client/src/components/Customer/Groceries/Groceries.jsx
+++ client/src/components/Customer/Groceries/Groceries.jsx
@@ -6,23 +6,21 @@ import Form from "react-bootstrap/Form";
 import Modal from "react-bootstrap/Modal";
 import image from "./images/image-20200921082601-1.jpeg";
 import "./Groceries.scss";
+import { NumericFormat } from "react-number-format";
 
 function Groceries() {
-
-
   const [list, setList] = useState([
     {
-      id: '',
+      id: "",
       img: "https://anhgaixinh.biz/wp-content/uploads/2022/01/gai-xinh-mac-vay-xep-ly-ngan-9.jpg",
       attribute: "",
       price: 0,
       amount: 0,
       note: "",
       totalPrice: 0,
-    }
+    },
   ]);
   const [show, setShow] = useState(false);
-
 
   // Danh sách các sản phẩm
   const handleClose = () => setShow(false);
@@ -30,27 +28,27 @@ function Groceries() {
 
   const handleOnIncrease = (i, e) => {
     const increase = [...list];
-    increase[i]['amount'] = parseInt(increase[i]['amount']) + 1;
-    increase[i]['totalPrice'] = increase[i]['amount'] * increase[i]['price'];
-    setList(increase)
+    increase[i]["amount"] = parseInt(increase[i]["amount"]) + 1;
+    increase[i]["totalPrice"] = increase[i]["amount"] * increase[i]["price"];
+    setList(increase);
   };
   const handleOnReduced = (i) => {
     const count = [...list];
-    if(count[i]['amount'] <=0){
-       count[i]['amount'] = 0;
-       count[i]['totalPrice'] = count[i]['amount'] * count[i]['price'];
-    }else {
-      count[i]['amount'] = count[i]['amount'] - 1; 
-      count[i]['totalPrice'] = count[i]['amount'] * count[i]['price'];
+    if (count[i]["amount"] <= 0) {
+      count[i]["amount"] = 0;
+      count[i]["totalPrice"] = count[i]["amount"] * count[i]["price"];
+    } else {
+      count[i]["amount"] = count[i]["amount"] - 1;
+      count[i]["totalPrice"] = count[i]["amount"] * count[i]["price"];
     }
-    setList(count);    
+    setList(count);
   };
 
   const handleOnClickAddMore = (e) => {
     let newList = [...list];
     const newId = newList.length + 1;
     newList = {
-      id:  '',
+      id: "",
       img: "https://anhgaixinh.biz/wp-content/uploads/2022/01/gai-xinh-mac-vay-xep-ly-ngan-10.jpg",
       attribute: "",
       price: 0,
@@ -61,14 +59,37 @@ function Groceries() {
     setList([...list, newList]);
   };
 
-  const changeInp =(i, e)=>{
-    const val = [...list];
-    val[i][e.target.name]=e.target.value;
-    val[i]['totalPrice']=val[i]['price'] * val[i]['amount'];
-    setList(val);
+  //In ra tổng tiền
+
+  var total = 0;
+  var totalOrderCost = 0;
+  var orderCost = 0;
+  for (var li of list) {
+    total += li.totalPrice;
+    if (total <= 2000000) {
+      orderCost += li.totalPrice * (3 / 100);
+    }
+    if (total <= 20000000) {
+      orderCost += (1 / 2 / 100) * li.totalPrice;
+    }
+    if (total <= 100000000) {
+      orderCost += li.totalPrice * (2 / 100);
+    }
+    if (total > 100000000) {
+      orderCost += li.totalPrice * (1 / 100);
+    }
+    totalOrderCost += total + orderCost;
   }
 
-  
+ 
+
+  const changeInp = (i, e) => {
+    const val = [...list];
+    val[i][e.target.name] = e.target.value;
+    val[i]["totalPrice"] = val[i]["price"] * val[i]["amount"];
+    setList(val);
+  };
+
   return (
     <>
       <div className="groceries">
@@ -86,69 +107,9 @@ function Groceries() {
             </tr>
           </thead>
           <tbody>
-            {/* <tr>
-              <td className="pt-5">1</td>
-              <td>
-                <img src={image} alt="knife" className="image mt-4"></img>
-              </td>
-              <td>
-                <input
-                  className="w-100"
-                  type="text"
-                  placeholder="Tên sản phẩm"
-                />
-                <textarea
-                  className="mt-2 attribute w-100"
-                  type="text"
-                  placeholder="Màu sắc, size, kích thước"
-                ></textarea>
-                <input
-                  className="w-100"
-                  type="text"
-                  placeholder="Link sản phẩm"
-                />
-              </td>
-              <td className="pt-5"> {price} </td>
-              <td className="soLuong">
-                <div className="d-flex soLuong">
-                  <div
-                    className="tru border border-dark px-3"
-                    onClick={(e) => handleOnReduced(e)}
-                  >
-                    {" "}
-                    -{" "}
-                  </div>
-                  <input
-                    className="value border w-50 border-dark px-3 text-center"
-                    type="text"
-                    value={count}
-                    onChange={(e) => handleOnChangeAmount(e)}
-                  />
-                  <div
-                    className="cong border border-dark px-3"
-                    onClick={(e) => handleOnIncrease(e)}
-                  >
-                    {" "}
-                    +{" "}
-                  </div>
-                </div>
-              </td>
-              <td>
-                {" "}
-                <textarea
-                  className="ghi_chu"
-                  name=""
-                  id=""
-                  cols="30"
-                  rows="10"
-                  placeholder="Ghi chú sản phẩm..."
-                ></textarea>{" "}
-              </td>
-              <td className="pt-5"> {totalPrice} </td>
-            </tr> */}
             {list.map((li, i) => (
               <tr key={i}>
-                <td className="pt-5"> {i+1} </td>
+                <td className="pt-5"> {i + 1} </td>
                 <td>
                   <img
                     style={{ width: "96px", height: "64px", marginTop: "24px" }}
@@ -172,7 +133,14 @@ function Groceries() {
                     placeholder="Link sản phẩm"
                   />
                 </td>
-                <td className="pt-5"> <input type="text" name="price" onChange={(e)=>changeInp(i, e)} /></td>
+                <td className="pt-5">
+                  {" "}
+                  <input
+                    type="text"
+                    name="price"
+                    onChange={(e) => changeInp(i, e)}
+                  />
+                </td>
                 <td className="soLuong">
                   <div className="d-flex soLuong">
                     <div
@@ -186,7 +154,8 @@ function Groceries() {
                       className="value border w-50 border-dark px-3 text-center"
                       type="text"
                       value={li.amount}
-                      name="amount" onChange={(e)=>changeInp(i, e)}
+                      name="amount"
+                      onChange={(e) => changeInp(i, e)}
                     />
                     <div
                       className="cong border border-dark px-3"
@@ -208,59 +177,145 @@ function Groceries() {
                     placeholder="Ghi chú sản phẩm..."
                   ></textarea>{" "}
                 </td>
-                <td className="pt-5"> {li.price * li.amount} </td>
+                <td className="pt-5"> {li.totalPrice} </td>
               </tr>
             ))}
           </tbody>
         </Table>
 
-        <div className="addMore">
-          <button onClick={(e) => handleOnClickAddMore(e)}>
-            Thêm sản sản phẩm
-          </button>
+        <div className="container d-flex justify-content-between mt-4">
+          <div className="form_custom">
+            <div className="addMore">
+              <button
+                style={{ backgroundColor: "#8610e8", border: "none" }}
+                className="py-2 px-1 rounded text-white"
+                onClick={(e) => handleOnClickAddMore(e)}
+              >
+                + Thêm sản sản phẩm
+              </button>
+            </div>
+            <div className="d-flex flex-column w-50">
+              <label htmlFor="" className="">
+                <h5>Địa chỉ kho Trung Quốc</h5>
+              </label>
+              <select name="" id="" className="p-1">
+                <option value="" className="text-center">
+                  --Lựa chọn kho--
+                </option>
+                <option value="quangChau">Quảng Châu</option>
+                <option value="dongHung">Đông Hưng</option>
+              </select>
+            </div>
+            <div className="form">
+              <p className="title-label">Thông Tin Khách Hàng</p>
+              <Container className="customer-form">
+                <Row>
+                  <Form.Label className="customer-title">Họ Tên</Form.Label>
+                  <Form.Control
+                    className="customer-field"
+                    type="text"
+                    placeholder="Nhập Họ Tên"
+                  />
+                </Row>
+                <Row>
+                  <Form.Label className="customer-title">
+                    Số Điện Thoại
+                  </Form.Label>
+                  <Form.Control
+                    className="customer-field"
+                    type="text"
+                    placeholder="Nhập Số Điện Thoại"
+                  />
+                </Row>
+                <Row>
+                  <Form.Label className="customer-title">Địa chỉ</Form.Label>
+                  <Form.Select className="customer-field">
+                    <option>Vui Lòng Chọn Địa Chỉ</option>
+                  </Form.Select>
+                </Row>
+              </Container>
+            </div>
+            <Button variant="warning" className="end-btn">
+              Tạo Đơn Hàng
+            </Button>
+          </div>
+          <div
+            style={{
+              width: "360px",
+              height: "100%",
+              backgroundColor: "#f9f9f9",
+            }}
+            className="border border-danger p-2"
+          >
+            <div className="d-flex justify-content-between">
+              <p>Tổng tiền đặt hàng: </p>
+              <p className="">
+                {" "}
+                <NumericFormat
+                  disabled={true}
+                  style={{
+                    border: "none",
+                    textAlign: "right",
+                    backgroundColor: "#f9f9f9",
+                    width: "100px",
+                  }}
+                  value={total}
+                  thousandSeparator=","
+                />{" "}
+                đ
+              </p>
+            </div>
+            <div className="d-flex justify-content-between">
+              <div className="d-flex">
+                <p>Phí đặt hàng</p>
+                <span
+                  style={{
+                    fontSize: "16px",
+                    paddingTop: "6px",
+                    color: "#005e91",
+                  }}
+                  class="material-symbols-outlined mx-1"
+                >
+                  help
+                </span>
+                :
+              </div>
+              <p className="">
+                {" "}
+                <NumericFormat
+                  disabled={true}
+                  style={{
+                    border: "none",
+                    textAlign: "right",
+                    backgroundColor: "#f9f9f9",
+                    width: "100px",
+                  }}
+                  value={orderCost}
+                  thousandSeparator=","
+                />{" "}
+                đ
+              </p>
+            </div>
+            <div className="d-flex justify-content-between">
+              <p className="">Tổng tiền/chưa có phí ship TQ: </p>
+              <p className="">
+                {" "}
+                <NumericFormat
+                  disabled={true}
+                  style={{
+                    border: "none",
+                    textAlign: "right",
+                    backgroundColor: "#f9f9f9",
+                    width: "100px",
+                  }}
+                  value={totalOrderCost}
+                  thousandSeparator=","
+                />{" "}
+                đ
+              </p>
+            </div>
+          </div>
         </div>
-        <div className="d-flex flex-column w-50">
-          <label htmlFor="" className="">
-            <h5>Địa chỉ kho Trung Quốc</h5>
-          </label>
-          <select name="" id="" className="p-1">
-            <option value="" className="text-center">
-              --Lựa chọn kho--
-            </option>
-            <option value="quangChau">Quảng Châu</option>
-            <option value="dongHung">Đông Hưng</option>
-          </select>
-        </div>
-        <div className="form">
-          <p className="title-label">Thông Tin Khách Hàng</p>
-          <Container className="customer-form">
-            <Row>
-              <Form.Label className="customer-title">Họ Tên</Form.Label>
-              <Form.Control
-                className="customer-field"
-                type="text"
-                placeholder="Nhập Họ Tên"
-              />
-            </Row>
-            <Row>
-              <Form.Label className="customer-title">Số Điện Thoại</Form.Label>
-              <Form.Control
-                className="customer-field"
-                type="text"
-                placeholder="Nhập Số Điện Thoại"
-              />
-            </Row>
-            <Row>
-              <Form.Label className="customer-title">Địa chỉ</Form.Label>
-              <Form.Select className="customer-field">
-                <option>Vui Lòng Chọn Địa Chỉ</option>
-              </Form.Select>
-            </Row>
-          </Container>
-        </div>
-        <Button variant="warning" className="end-btn">
-          Tạo Đơn Hàng
-        </Button>
       </div>
       <Modal show={show} onHide={handleClose} animation={false} size="lg">
         <Modal.Header closeButton>
