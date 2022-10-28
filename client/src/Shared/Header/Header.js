80a9fac84a58553334caa6539ddd0a80fb3d4abc
ThienBk <thiennguyen2000qwer@gmail.com>
2022-10-28 20:10:10 +0700

    dsz

--- client/src/Shared/Header/Header.js
+++ client/src/Shared/Header/Header.js
@@ -9,44 +9,35 @@ import "./Header.scss";
 import LOGO from '../../assets/public/img/logo-logistic-footer.png';
 import { Dropdown } from "react-bootstrap";
 import { AppContext } from "../../contexts/AppContextProvider";
-import {useNavigate} from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 
 function Header() {
-    const {logout}= useContext(AppContext)
+    const { logout } = useContext(AppContext)
     const naviagte = useNavigate()
-    const handleLogout = async() => {
-      await logout();
-      naviagte('/login')
+    const handleLogout = async () => {
+        await logout();
+        naviagte('/login')
     }
     return (
         <div className="Header">
-            <div className="container">
-                <div className="row">
-                    <div className="col-sm-6">
-                        <Navbar expand="lg" className="navbar navbar-expand-lg">
-                            <div className="logo">
-                                <img src={LOGO} />
-                            </div>
-                            <Nav className="end nav navbar-">
-                                <TiShoppingCart className="icon1" />
-                                <MdEmail className="icon1" />
-                                <AiOutlineBell className="icon1" />
-                                <Dropdown>
-                                    <Dropdown.Toggle variant="success" id="dropdown-basic">
-                                    <FaUserCircle className="icon2" />
-                                    </Dropdown.Toggle>
-                                    <Dropdown.Menu>
-                                        <Dropdown.Item onClick={handleLogout}>Log out</Dropdown.Item>
-                                    </Dropdown.Menu>
-                                </Dropdown>
-                            </Nav>
-                        </Navbar>
-                    </div>
-                    <div className="col-md-6">
-
-                    </div>
+            <Navbar expand="lg" className="navbar navbar-expand-lg">
+                <div className="logo">
+                    <img src={LOGO} />
                 </div>
-            </div>
+                <Nav className="end nav navbar-">
+                    <TiShoppingCart className="icon1" />
+                    <MdEmail className="icon1" />
+                    <AiOutlineBell className="icon1" />
+                    <Dropdown>
+                        <Dropdown.Toggle variant="success" id="dropdown-basic">
+                            <FaUserCircle className="icon2" />
+                        </Dropdown.Toggle>
+                        <Dropdown.Menu>
+                            <Dropdown.Item onClick={handleLogout}>Log out</Dropdown.Item>
+                        </Dropdown.Menu>
+                    </Dropdown>
+                </Nav>
+            </Navbar>
         </div>
     )
 }
